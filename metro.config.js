93c45d57b4fbc3ae20692d4ae1943bdd9e4c1226
Minh Anh <minhanhng.sofl@gmail.com>
2022-11-29 23:47:26 +0700

    Update metro config

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('@expo/metro-config');
 
-const defaultConfig = getDefaultConfig("./firebaseConfig.js");
+const defaultConfig = getDefaultConfig(".");
 defaultConfig.resolver.assetExts.push('cjs');
 
 module.exports = defaultConfig;
