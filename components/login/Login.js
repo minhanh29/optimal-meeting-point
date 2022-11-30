4c675212d05cf333772a43cc214ead5940d8f75f
Quynh (May) Tran <s3872104@rmit.edu.vn>
2022-11-30 11:55:55 +0700

    login UI

--- components/login/Login.js
+++ components/login/Login.js
@@ -3,45 +3,51 @@ import React, { useState } from "react";
 import {StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity} from "react-native";
  
 export default function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
- 
-  return (
-    <View style={styles.container}>
-      {/* <Image style={styles.image} source={require("./../images/logo.png")} /> */}
- 
-      <StatusBar style="auto" />
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Username"
-          placeholderTextColor="#B4BABC"
-          onChangeText={(username) => setUsername(username)}
-        />
-      </View>
- 
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Password"
-          placeholderTextColor="#B4BABC"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        />
-      </View>
- 
-      <TouchableOpacity>
-        <Text>Don't have an account?</Text>
-        <Text style={styles.signUp_button}>Sign Up!</Text>
-      </TouchableOpacity>
+    const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
 
-      <TouchableOpacity style={styles.loginBtn}>
-        <Text style={styles.loginText}>LOGIN</Text>
-      </TouchableOpacity>
+	const [fontsLoaded] = useFonts({
+		'Montserrat': require('./../../assets/fonts/Montserrat-Regular.ttf'),
+		'Montserrat-Italic': require('./../../assets/fonts/Montserrat-Italic-VariableFont_wght.ttf'),
+		'Montserrat-Bold': require('./../../assets/fonts/Montserrat-Bold.ttf'),
+		'jsMath-cmbx10': require('./../../assets/fonts/jsMath-cmbx10.ttf')
+	});
 
-      <TouchableOpacity>
-        <Text style={styles.forgot_button}>Forgot Password?</Text>
-      </TouchableOpacity>
+	if (!fontsLoaded)
+	return null
+
+	return (
+		<View style={styles.container}>
+		 <Image style={styles.image} source={require("./../../images/logo.png")} />
+		 <Text style={styles.appName}>OMP</Text>
+	
+		 <StatusBar style="auto" />
+		<View style={styles.inputView}>
+			<TextInput
+			style={styles.TextInput}
+			placeholder="Username"
+			placeholderTextColor="#B4BABC"
+			onChangeText={(username) => setUsername(username)}
+			/>
+		</View>
+	
+		<View style={styles.inputView}>
+			<TextInput
+			style={styles.TextInput}
+			placeholder="Password"
+			placeholderTextColor="#B4BABC"
+			secureTextEntry={true}
+			onChangeText={(password) => setPassword(password)}
+			/>
+		</View>
+
+		<TouchableOpacity style={styles.loginBtn}>
+			<Text style={styles.loginText}>Log In</Text>
+		</TouchableOpacity>
+
+		<TouchableOpacity>
+			<Text style={styles.signUp_button}>Don't have an account? Sign Up!</Text>
+		</TouchableOpacity> 
     </View>
   );
 }
@@ -49,44 +55,82 @@ export default function Login() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#EDF4F7",
     alignItems: "center",
     justifyContent: "center",
   },
- 
+
+  appName: {
+	color:"#9CC7CA",
+	marginBottom: 15,
+	fontSize: 50,
+	fontFamily: 'jsMath-cmbx10',
+	shadowColor: '#000',
+	shadowOffset: {
+		width: 0,
+		height:4,
+	},
+	shadowOpacity: 0.25,
+	elevation:20
+
+  },
+
   image: {
-    marginBottom: 40,
+    marginBottom: 15,
+	width: 150,
+	height: 150
   },
  
   inputView: {
-    backgroundColor: "#FFC0CB",
+    backgroundColor: "#FAFCFD",
     borderRadius: 30,
     width: "70%",
     height: 45,
     marginBottom: 20,
- 
-    alignItems: "center",
+	fontFamily: 'Montserrat',
+	shadowColor: 'black',
+        shadowOpacity: 0.2,
+        shadowOffset:{
+            width: 1,
+            height: 4,
+        }
   },
  
   TextInput: {
+	width: '100%',
     height: 50,
     flex: 1,
-    padding: 10,
+    padding: 10, //swift right by 10
     marginLeft: 20,
+	fontFamily: 'Montserrat',
   },
  
-  forgot_button: {
-    height: 30,
-    marginBottom: 30,
+  signUp_button: {
+	fontFamily:'Montserrat',
+	fontSize: 14,
+	marginBottom: 30,
+	marginTop: 10,
   },
- 
+
   loginBtn: {
-    width: "80%",
+    width: "70%",
     borderRadius: 25,
     height: 50,
     alignItems: "center",
     justifyContent: "center",
     marginTop: 40,
     backgroundColor: "#EE6548",
+	shadowColor: '#000',
+	shadowOffset: {
+		width: 0,
+		height:4,
+	},
+	shadowOpacity: 0.25,
+	elevation:20
   },
+
+  loginText: {
+	color: "#FAFCFD",
+	fontFamily: 'Montserrat-Bold',
+  }
 });
