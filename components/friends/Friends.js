8f6a312d053fef456edcbdb419fc17a6336d3d0a
Quynh (May) Tran <s3872104@rmit.edu.vn>
2022-12-09 11:50:33 +0700

    friend request

--- components/friends/Friends.js
+++ components/friends/Friends.js
@@ -1,11 +1,34 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, View, Button } from "react-native";
 import React, { useCallback, useMemo, useRef } from "react";
+import { Avatar, Box, Stack, Icon, Text, Flex, Spacer } from "@react-native-material/core";
+import { db } from "../../firebaseConfig"
+import { collection, getDocs } from "firebase/firestore";
+import { useDispatch } from "react-redux";
+import { useTheme } from '@react-navigation/native';
+import AIcon from "@expo/vector-icons/AntDesign";
+// import styles from "./styles";
 
-const Friends = () => {
+const Friends = ({navigate}) => {
+  const { colors } = useTheme();
   return (
-    <View>
-      <Text>Friends List</Text>
-    </View>
+    <Stack backgroundColor={colors.background}
+    h="100%"
+    w="100%"
+    items="center"
+    paddingTop={50}
+    spacing={20}
+    >
+      <Box elevation={4}
+      backgroundColor="white"
+      >
+        <Flex w="100%"
+        items="center"
+        direction="row">
+          <AIcon name="seach1" size={24} color="#C2C2C2"/>
+        </Flex>
+      </Box>
+
+    </Stack>
   );
 };
 
