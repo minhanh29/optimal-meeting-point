ef1a4637be0edbbfd2bc86d43f8fb73787bfafc6
sangnguyen0403 <sangnguyen04032002@gmail.com>
2022-12-17 15:16:47 +0700

    Style autocomplete bar

--- components/address/SetAddress.js
+++ components/address/SetAddress.js
@@ -4,17 +4,17 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import { useTheme } from '@react-navigation/native';
-import { Stack } from '@react-native-material/core'
+import { Box, Flex, IconButton, Stack, VStack } from '@react-native-material/core'
 import { useRef } from 'react'
+import Icon from "@expo/vector-icons/Ionicons";
+import FIcon from "@expo/vector-icons/Feather";
 import Config from "react-native-config";
 
 const SetAddress = () => {
   const { colors } = useTheme();
   const [addressText, setAddressText] = useState({})
   console.log("Ađress", addressText)
-  const ref = useRef()
-
-
+  console.log("KEY", Config.GOOGLE_MAPS_API_KEY)
 
   return (
     <Stack
@@ -24,41 +24,50 @@ const SetAddress = () => {
       items="center"
       paddingTop={35}
     >
-    <View style={{...styles.searchContainer,marginTop: Platform.OS == "ios" ? 50 : 40 }}>
-      <GooglePlacesAutocomplete
-        style={{flex: 1}}
-        placeholder='Search Location'
-        keepResultsAfterBlur = {true}
-        onPress={(data, details) => {
-          console.log(details)
-          setAddressText({
-            location: details.geometry.location,
-            address: details.formatted_address,
-          })
-        }}
-        minLength={2}
-        fetchDetails={true}
-        // GooglePlacesDetailsQuery={{
-        //   fields: ['formatted_address', 'geometry'],
-        // }}
-        // renderRow={(rowData) => {
-        //   const title = rowData.structured_formatting.main_text;
-        //   const address = rowData.structured_formatting.secondary_text
-        //   return(
-        //     <View>
-        //        <Text>{title}</Text>
-        //        <Text style={styles.locationAddress}>{address}</Text>
-        //     </View>
-        //   )
-        // }}
-        query={{
-          key: Config.GOOGLE_MAPS_API_KEY,
-          language: 'en',
-          components: 'country:vn'  // Limit to only Vietnam
-        }}
-      />
+      <View style={{ ...styles.searchContainer, marginTop: Platform.OS == "ios" ? 50 : 40 }}>
+        <GooglePlacesAutocomplete
+          style={{ flex: 1 }}
+          placeholder='Search Location'
+          keepResultsAfterBlur={true}
+          onPress={(data, details) => {
+            console.log(details)
+            setAddressText({
+              location: details.geometry.location,
+              address: details.formatted_address,
+            })
+          }}
+          minLength={2}
+          fetchDetails={true}
+          // GooglePlacesDetailsQuery={{
+          //   fields: ['formatted_address', 'geometry'],
+          // }}
+          renderRow={(rowData) => {
+            const title = rowData.structured_formatting.main_text;
+            const address = rowData.structured_formatting.secondary_text
+            return (
+              <View style={{width: '100%'}}>
+                <Flex direction='row' width='100%'>
+                  <Box style={styles.iconContainer}>
+                    <Icon name="location-sharp" size={24} style={styles.iconStyle} />
+                  </Box>
+                  <VStack style={styles.textContainer}>
+                    <Text style={styles.titleText}>{title}</Text>
+                    <Text style={styles.locationAddress}>{address}</Text>
+                  </VStack>
+                </Flex>
+              </View>
+            )
+          }}
+          query={{
+            // key: Config.GOOGLE_MAPS_API_KEY,
+            key: "AIzaSyAmdFtD0mKY5OkanGXSU6hR_-el1hKSpL4",
+            language: 'en',
+            components: 'country:vn'  // Limit to only Vietnam
+          }}
+          enablePoweredByContainer={false}
+        />
 
-    </View>
+      </View>
     </Stack>
 
 
--- components/address/styles.js
+++ components/address/styles.js
@@ -3,11 +3,11 @@ import globalStyles from "../../styles"
 
 const styles = StyleSheet.create({
   ...globalStyles,
-	searchContainer:{
+  searchContainer: {
     position: 'absolute',
     width: '80%',
     shadowColor: "black",
-    shadowOffset: {width: 2, height: 2},
+    shadowOffset: { width: 2, height: 2 },
     shadowOpacity: 0.5,
     shadowRadius: 4,
     elevation: 4,
@@ -15,14 +15,15 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     borderRadius: 15,
   },
-  locationAddress:{
+  locationAddress: {
     marginTop: 5,
+    width: '100%',
   },
-  searchContainer:{
+  searchContainer: {
     backgroundColor: 'white',
     borderRadius: 15,
     position: 'absolute',
-    width: '80%',
+    width: '100%',
     // shadowColor: 'black',
     // shadowOffset: {width:2, height: 2},
     // shadowOpacity: 0.5,
@@ -31,8 +32,35 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 8,
     borderRadius: 8,
-},
+    height: Dimensions.get("window").height,
+  },
 
+  iconContainer:{
+    width: 40,
+    height: 40,
+    borderRadius: 40/2,
+    display: 'flex',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#E8E8E8',
+  },
+
+  iconStyle: {
+    color:"#F6CF65",
+    padding: 6,
+    width: 36,
+    height: 36,
+    borderRadius: 36/2,
+  },
+  textContainer:{
+    marginLeft: 20,
+    width: '80%',
+    overflow: "hidden",
+  },  
+  titleText:{
+    fontWeight: "bold",
+  },
 });
 
 export default styles;
