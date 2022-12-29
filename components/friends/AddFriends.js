407aa35053868897a04022f53cf0626c7b3450c5
Quynh (May) Tran <s3872104@rmit.edu.vn>
2022-12-29 17:10:21 +0700

    friend request works only once after logged in

--- components/friends/AddFriends.js
+++ components/friends/AddFriends.js
@@ -19,55 +19,56 @@ const AddFriends = ({navigation}) => {
 
     const {colors} = useTheme();
     const [userList, setUserList] = useState([]);
-    const [friendList , setFriendList] = useState([]);
-    const [requestList, setRequestList] = useState([]);
+    // const [friendList , setFriendList] = useState([]);
+    // const [requestList, setRequestList] = useState([]);
     const [selectedIndex, setSelectedIndex] = useState([]);
     const user = useSelector(selectUser);
     const [avatar, setAvatar] = useState(null);
     const dispatch = useDispatch()
 
+    // const fetchFriendInfo = async () => {
+    //     try {
+    //         const querySnapshot = await getDocs(collection(db, "friend"));
+    //         const result = []
+    //         querySnapshot.forEach(doc => {
+    //             result.push({
+    //                 id: doc.id,
+    //                 ...doc.data()
+    //             })
+    //         })
+    //         setFriendList(result)
+    //     } catch (e) {
+    //         console.log(e)
+    //     }
+    // }
 
-    const fetchFriendInfo = async () => {
-        try {
-            const querySnapshot = await getDocs(collection(db, "friend"));
-            const result = []
-            querySnapshot.forEach(doc => {
-                result.push({
-                    id: doc.id,
-                    ...doc.data()
-                })
-            })
-            setFriendList(result)
-        } catch (e) {
-            console.log(e)
-        }
-    }
-
-    const fetchRequestInfo = async () => {
-        try {
-            const querySnapshot = await getDocs(collection(db, "friend_request"));
-            const result = []
-            querySnapshot.forEach(doc => {
-                result.push({
-                    id: doc.id,
-                    ...doc.data()
-                })
-            })
-            setRequestList(result)
-        } catch (e) {
-            console.log(e)
-        }
-    }
+    // const fetchRequestInfo = async () => {
+    //     try {
+    //         const querySnapshot = await getDocs(collection(db, "friend_request"));
+    //         const result = []
+    //         querySnapshot.forEach(doc => {
+    //             result.push({
+    //                 id: doc.id,
+    //                 ...doc.data()
+    //             })
+    //         })
+    //         setRequestList(result)
+    //     } catch (e) {
+    //         console.log(e)
+    //     }
+    // }
 
     const fetchUserInfo = async () => {
         try {
             const querySnapshot = await getDocs(collection(db, "user"));
             const result = []
             querySnapshot.forEach(doc => {
-                result.push({
-                    id: doc.id,
-                    ...doc.data()
-                })
+                if (doc.id !== user.user.id ) {
+                    result.push({
+                        id: doc.id,
+                        ...doc.data()
+                    })
+                }
             })
 
             const doc = querySnapshot.docs[0]
@@ -76,43 +77,7 @@ const AddFriends = ({navigation}) => {
             // setUsername(data.username)
             setAvatar(data.ava_url)
             setUserList(result)
-<<<<<<< HEAD
-            // console.log(userList)
-            // setUserList(temp => {
-            //     for (let i = 0; i<= userList.length; i++) {
-            //         if (user.user.id == friendList.person1_id || user.user.id == requestList.sender_id) {
-            //             const isIncludeFriend = userList.includes(friendList.person2_id)
-            //             const isIncludeRequest = userList.includes(requestList.receiver_id)
-            //             if (isIncludeFriend || isIncludeRequest) {
-            //                 return userList.filter(item => item !== friendList.person2_id &&
-            //                      item !== user.user.id && item !== requestList.receiver_id)
-            //             } else {
-            //                 return [...temp]
-            //             }
-            //         } else {
-            //             continue
-            //         }
-            //     }
-            // })
-            console.log(friendList)
-
             
-=======
-            setUserList(temp => {
-                if (user.user.id == friendList.person1_id || user.user.id == requestList.sender_id) {
-                    const isIncludeFriend = userList.includes(friendList.person2_id)
-                    const isIncludeRequest = userList.includes(requestList.receiver_id)
-                    if (isIncludeFriend || isIncludeRequest) {
-                        return userList.filter(item => item !== friendList.person2_id &&
-                                item !== user.user.id && item !== requestList.receiver_id)
-                    } else {
-                        return [...temp]
-                    }
-                } else {
-                    return [...temp]
-                }
-            })
->>>>>>> b77c65a1564c21f2df75a65f0b79fc7c9046b8e4
         } catch (e) {
             console.log(e)
         }
@@ -120,13 +85,13 @@ const AddFriends = ({navigation}) => {
 
     useEffect(() => {
         fetchUserInfo();
-        fetchFriendInfo();
-        fetchRequestInfo();
+        // fetchFriendInfo();
+        // fetchRequestInfo();
     }, []);
 
     const handleAdd = (receiver, index) => {
-        user = useSelector(selectUser)
-        
+        // user = useSelector(selectUser)
+
         setSelectedIndex(prev => {
             const isInclude = selectedIndex.includes(index)
             if (isInclude) {
@@ -143,16 +108,12 @@ const AddFriends = ({navigation}) => {
                 receiver_id: receiver.id
             }
             console.log(data)
-            console.log(user.id)
             dispatch(addFriendAsync(data))
         } catch (e) {
             console.log(e)
         }
     }
 
-    // const sendRequest = async () => {
-        
-    // }
 
     return (
         <View>
