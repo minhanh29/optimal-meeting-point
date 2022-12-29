3f19ba5fa26e36667f0ee5142f92bc95e33915a5
Minh Anh <s3927589@gmail.com>
2022-12-29 23:19:09 +0700

    Change config

--- babel.config.js
+++ babel.config.js
@@ -3,20 +3,20 @@ module.exports = function (api) {
 	return {
 		presets: ["babel-preset-expo"],
 		plugins: [
-			"react-native-reanimated/plugin",
-			[
-				"module:react-native-dotenv",
-				{
-					envName: "APP_ENV",
-					moduleName: "@env",
-					path: ".env",
-					blocklist: null,
-					allowlist: null,
-					safe: false,
-					allowUndefined: true,
-					verbose: false,
-				},
-			],
+			// "react-native-reanimated/plugin",
+			// [
+			// 	"module:react-native-dotenv",
+			// 	{
+			// 		envName: "APP_ENV",
+			// 		moduleName: "@env",
+			// 		path: ".env",
+			// 		blocklist: null,
+			// 		allowlist: null,
+			// 		safe: false,
+			// 		allowUndefined: true,
+			// 		verbose: false,
+			// 	},
+			// ],
 		],
 	};
 };
