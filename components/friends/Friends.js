7fc07a2f9de17e78b752cc52d13799a56461f8f0
Han0910 <s3894290@rmit.edu.vn>
2023-01-02 21:47:51 +0700

    Done cloud functions

--- components/friends/Friends.js
+++ components/friends/Friends.js
@@ -1,9 +1,9 @@
 import { StyleSheet, View, Button, TextInput } from "react-native";
-import React, { useCallback, useMemo, useRef } from "react";
+import React, { useCallback, useMemo, useRef, useEffect } from "react";
 import { Avatar, Box, Stack, Icon, Text, Flex, Spacer, IconButton } from "@react-native-material/core";
 import { db } from "../../firebaseConfig"
 import { collection, getDocs } from "firebase/firestore";
-import { useDispatch, useSelector } from "react-redux";
+import { connect, useDispatch, useSelector } from "react-redux";
 import { useTheme } from '@react-navigation/native';
 import AIcon from "@expo/vector-icons/AntDesign";
 // import Icon from "@expo/vector-icons/Ionicons";
@@ -11,8 +11,78 @@ import FIcon from "@expo/vector-icons/Feather";
 import MIcon from "@expo/vector-icons/MaterialIcons"
 import styles from "./styles";
 import { selectUser } from "../../redux/reducers/userSlice";
+import { InstantSearch, connectRefinementList, connectHits } from "react-instantsearch-hooks";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 
-const Friends = ({navigation}) => {
+const FriendFilter = connectRefinementList(({items, refine, user_id}) => {
+  console.log("User Id", user_id)
+  useEffect(() => {
+    refine(user_id)
+  }, [user_id])
+  return <View></View>
+})
+
+const FriendHits = connectHits(({hits, navigation}) => {
+  const { colors } = useTheme();
+
+  const dataList = hits.map(item => ({
+    id: item.objectID,
+    name: item.name,
+    person1_id: item.person1_id,
+    ava_url: item.ava_url,
+    username: item.username,
+    person2_id: item.person2_id
+  }))
+
+  return (
+    <Stack
+      backgroundColor={colors.background}
+      h="100%"
+      w="100%"
+      items="center"
+      paddingTop={20}
+    >
+      <FlatList
+      style={styles.listContainer}
+      data={dataList}
+      keyExtractor={item => item.objectID}
+      renderItem = {({item}) => (
+        <Stack w='100%' spacing={20} mb={10}>
+          <TouchableOpacity
+            elevation={4}
+            backgroundColor="white"
+            style={styles.groupCardContainer}
+            w='100%'>
+              <Flex
+                w="100%"
+                items="center"
+                direction="row">
+                  <Stack
+                    style={{marginLeft: 17}}
+                    spacing={5}
+                    w="70%">
+                       <Avatar
+                          label={item.name}
+                          icon={props => <Icon name="account" {...props} />}
+                          image={item.ava_url ? { uri: item.ava_url } : null}
+                          imageStyle={{ borderRadius: 10 }}
+                      />
+                      <Text style={styles.cardHeader}>
+                          {item.name}
+                      </Text>
+                      <Text style={styles.infoContent}>
+                          @{item.username}
+                      </Text>
+                  </Stack>
+              </Flex>
+          </TouchableOpacity>
+        </Stack>
+      )}>
+      </FlatList>
+    </Stack>
+  )
+})
+const Friends = ({hits, navigation}) => {
   const { colors } = useTheme();
   const user = useSelector(selectUser);
 
@@ -23,25 +93,18 @@ const Friends = ({navigation}) => {
       w="100%"
       items="center"
       paddingTop={35}
-
       >
-
           <Flex w="85%" direction="row">
-			  <Flex w="90%" style={styles.searchHolder} direction="row">
-				<AIcon name="search1" style={styles.iconImg} color='B4BABC'/>
-				<TextInput
-				  style={styles.searchInput}
-				  placeholder='Search friends'
-				  color='#B4BABC'
-				/>
-			  </Flex>
+            <InstantSearch searchClient={searchClient} indexName="friends">
+                      <SearchBox searchBoxName= "Search group" />
+              <GroupFilter user_id={user.user.id} attribute="user_id"/>
+              <FriendHits/>
+            </InstantSearch>
             <IconButton
             icon={props => <AIcon name="adduser" {...props} color='B4BABC' />}
             onPress={() => navigation.navigate("AddFriends")}
             />
-
           </Flex>
-
       </Stack>
   );
 };
---
+++ components/friends/functions/index.js
@@ -0,0 +1,208 @@
+// Import all needed modules.
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const algoliasearch = require('algoliasearch');
+// const { query } = require('firebase/firestore');
+// const axios = require('axios').default;
+
+// Set up Firestore.
+admin.initializeApp();
+const db = admin.firestore();
+
+// Set up Algolia.
+const algoliaClient = algoliasearch(functions.config().algolia.appid, functions.config().algolia.apikey);
+const collectionIndex = algoliaClient.initIndex("friends");
+
+
+async function saveDocumentInAlgolia(record) {
+	return await collectionIndex.saveObject(record);
+}
+
+async function updateDocumentInAlgolia(change) {
+    const docBeforeChange = change.before.data()
+    const docAfterChange = change.after.data()
+    if (docBeforeChange && docAfterChange) {
+		await saveDocumentInAlgolia(change.after);
+    }
+}
+
+async function deleteDocumentFromAlgolia(snapshot) {
+    if (snapshot.exists) {
+        const objectID = snapshot.id;
+        await collectionIndex.deleteObject(objectID);
+    }
+}
+
+// Create a HTTP request cloud function.
+exports.sendCollectionToAlgolia = functions.region('asia-northeast1').https.onRequest(async (req, res) => {
+
+	try {
+		// This array will contain all records to be indexed in Algolia.
+		// A record does not need to necessarily contain all properties of the Firestore document,
+		// only the relevant ones.
+		const algoliaRecords = [];
+
+		// Retrieve all documents from the COLLECTION collection.
+		const querySnapshot1 = await db.collection('group').get();
+		const querySnapshot2 = await db.collection('groupNuser').get()
+
+		const groupNames = {}
+		querySnapshot1.docs.forEach(doc => {
+			const data = doc.data();
+			groupNames[doc.id] = data.group_name
+		});
+
+		querySnapshot2.docs.forEach(doc => {
+			const data = doc.data();
+			if (data.group_id.trim() !== "") {
+				const record = {
+					objectID: doc.id,
+					group_id: data.group_id,
+					user_id: data.user_id,
+					group_name: groupNames[data.group_id]
+				}
+
+				algoliaRecords.push(record);
+			}
+		})
+
+
+		// After all records are created, we save them to
+		collectionIndex.saveObjects(algoliaRecords, (_error, content) => {
+			res.status(200).send("COLLECTION was indexed to Algolia successfully.");
+		});
+	} catch (e) {
+		console.log(e.message)
+		res.status(500).send(e.message);
+	}
+})
+
+exports.groupWriteListener = functions.region('asia-northeast1').firestore
+	.document("groupNuser/{docID}")
+	.onCreate((snap, context) => {
+		console.log(snap.data())
+		const group_id = snap.data().group_id.trim()
+		console.log(group_id)
+		db.collection("group").doc(group_id).get().then((group_data) => {
+			const record = {
+				objectID: snap.id,
+				group_id: snap.data().group_id,
+				user_id: snap.data().user_id,
+				group_name: group_data.data().group_name,
+			}
+			console.log(group_data.data())
+			saveDocumentInAlgolia(record)
+			.then(res => console.log("Group is added to algolia", res))
+			.catch(e => console.log("Algolia Add Error", e.message))
+		})
+	})
+
+exports.groupUpdateFromGroupListener = functions.region('asia-northeast1').firestore
+	.document("group/{docID}")
+	.onUpdate((snap, context) =>{
+		console.log(snap.after.id)
+
+		db.collection("groupNuser").where("group_id", "==", snap.after.id).get().then((data) => {
+			data.docs.forEach((doc) => {
+				const record = {
+					objectID: doc.id,
+					group_id: doc.data().group_id,
+					user_id: doc.data().user_id,
+					group_name: snap.after.data().group_name,
+				}
+				console.log(record.group_name)
+				saveDocumentInAlgolia(record)
+				.then(res => console.log("Group info from group is updated to algolia", res))
+				.catch(e => console.log("Algolia Update Error", e.message))
+			})
+		})
+	})
+
+
+exports.groupDeleteListener = functions.region('asia-northeast1').firestore
+	.document("groupNuser/{docID}")
+	.onDelete((change, context) => {
+		deleteDocumentFromAlgolia(change)
+			.then(res => console.log("Group is deleted to algolia", res))
+			.catch(e => console.log("Algolia Delete Error", e.message))
+	})
+
+// friend
+exports.sendFriendsToAlgolia = functions.region('asia-northeast1').https.onRequest(async (req, res) => {
+	try{
+		const algoliaRecords = []
+
+		const querySnapshot = await db.collection('friend').get()
+
+		for(let i = 0; i < querySnapshot.docs.length; i++){
+			let doc = querySnapshot.docs[i]
+			const data = doc.data()
+			const person1_id = data.person1_id;
+
+			const person1 = await db.collection("user").doc(person1_id).get()
+			console.log("person 1",person1)
+			console.log("person 1 data ", person1.data())
+			if(person1.data() === undefined){
+				continue
+			}
+			const person1_info = {
+				person1_id: person1_id,
+				ava_url: person1.data().ava_url,
+				name: person1.data().name,
+				username: person1.data().username
+			};
+
+			const record = {
+				objectID: doc.id,
+				...person1_info,
+				person2_id: data.person2_id
+			};
+			console.log(record)
+			algoliaRecords.push(record);
+			collectionIndex.saveObjects(algoliaRecords, (_error, content) => {
+			res.status(200).send("COLLECTION was indexed to Algolia successfully.");
+			})
+		};
+ 	} catch (e) {
+		console.log(e.message)
+		res.status(500).send(e.message);
+	}
+})
+
+exports.friendUpdateListener = functions.region('asia-northeast1').firestore
+	.document("user/{docId}")
+	.onUpdate(async (change, context) => {
+		const person1 = change.after.data()
+		const querySnapshot = await db.collection('friend').where("person1_id", "==", change.after.id).get()
+		for(let i  = 0; i < querySnapshot.docs.length; i++){
+			let doc = querySnapshot.docs[i]
+			let data = doc.data()
+
+			if(person1 === undefined){
+				continue
+			}
+			const person1_info = {
+				person1_id: change.after.id,
+				ava_url: person1.ava_url,
+				name: person1.name,
+				username: person1.username
+			};
+			const record = {
+				objectID: doc.id,
+				...person1_info,
+				person2_id: data.person2_id
+			}
+			console.log("record", record)
+			saveDocumentInAlgolia(record)
+			.then(res => console.log("Group info from group is updated to algolia", res))
+			.catch(e => console.log("Algolia Update Error", e.message))
+		}
+	})
+
+exports.friendDeleteListenter = functions.region('asia-northeast1').firestore
+	.document("friend/{docID}")
+	.onDelete((change, context) => {
+		deleteDocumentFromAlgolia(change)
+			.then(res => console.log("Friend is deleted to algolia", res))
+			.catch(e => console.log("Algolia Delete Error", e.message))
+	})
